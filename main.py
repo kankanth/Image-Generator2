d3326a8876b83c6205b85702f73309c61151e9a4
kankanth <99627125+kankanth@users.noreply.github.com>
2026-04-01 21:33:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import requests
 
 from myserver import server_on
 
-TOKEN =  os.getenv("DISCORD_BOT_TOKEN")
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 HF_TOKEN = os.getenv("HF_API_TOKEN")
-
 
 API_URL = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
 headers = {"Authorization": f"Bearer {HF_TOKEN}"}
@@ -17,6 +16,9 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+
     if message.content.startswith("!img "):
         prompt = message.content[5:]
         await message.channel.send("กำลังสร้างภาพ...")
@@ -26,10 +28,10 @@ async def on_message(message):
         if response.status_code == 200:
             with open("output.png", "wb") as f:
                 f.write(response.content)
+
             await message.channel.send(file=discord.File("output.png"))
         else:
             await message.channel.send(response.text)
 
 server_on()
-
 client.run(TOKEN)
